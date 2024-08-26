d2fcd51d99c84f303f3a7647e2d8a5c3a6f19948
Pawan Sharma <pawansharma93520@gmail.com>
2024-08-27 00:19:30 +0530

    bugs remain

--- cmd/web/rander.go
+++ cmd/web/rander.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"html/template"
+	"net/http"
 )
 
 type templateData struct {
@@ -21,6 +23,27 @@ type templateData struct {
 
 var function = template.FuncMap{}
 
-
 var templateFS embed.FS
 
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	return td
+}
+
+func (app *application) renderTemplate(w http.ResponseWriter, r *http.Request, page string, td *templateData, partials ...string) error {
+	var t *template.Template
+	var err error
+	templateToRender := fmt.Sprintf("templates/%s.page.tmpl", page)
+
+	_, templateInMap := app.templateCache[templateToRender]
+
+	if app.config.env == "production" && templateInMap {
+		t = app.templateCache[templateToRender]
+	} else {
+		t, err = app.parseTemplate(partials, page, templateToRender)
+		if err != nil {
+			app.errorLog.Println(err)
+			return err
+		}
+	}
+	return nil
+}
